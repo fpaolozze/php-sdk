10a57fbf5262fe5f20fa476f02155ae2eadec0b5
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-16 15:33:18 -0300

    implement dele action

--- lib/Cards.php
+++ lib/Cards.php
@@ -5,8 +5,8 @@ namespace Paggi;
 class Cards
 {
 
-    use find, findById, insert, delete {
-        find::__construct as private __findConstruct;
+    use findAll, findById, insert, delete {
+        findAll::__construct as private __findConstruct;
         findById::__construct as private __findByIdConstruct;
         insert::__construct as private __insertConstruct;
         delete::__construct as private __deleteConstruct;
