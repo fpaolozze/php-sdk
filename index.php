5059179a797d962bac25999a784f33aef4c000db
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-16 14:21:10 -0300

    tests call, add devtoken and staging token to test

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 include __DIR__ . '/lib/Paggi.php';
 include __DIR__ . '/lib/Cards.php';
+include __DIR__ . '/lib/Card.php';
 include __DIR__ . '/lib/Charges.php';
 
 
@@ -9,8 +10,10 @@ use Paggi\Paggi;
 use Paggi\Cards;
 use Paggi\Card;
 
+$dev_token = "B31DCE74-E768-43ED-86DA-85501612548F";
+$staging_token = "d3606313-bc7e-428d-8254-ec83853bbd72";
 
-Paggi::init("d3606313-bc7e-428d-8254-ec83853bbd72",true);
+Paggi::init($dev_token,true);
 
 
 //$charges = new Charges();
@@ -24,13 +27,13 @@ Paggi::init("d3606313-bc7e-428d-8254-ec83853bbd72",true);
 $cards = new Cards();
 //$response2 = $cards->find();
 //echo ($response2);
-$response2 = $cards->findById("card_6d502929-c214-4c3f-8e4d-191bd42edafa");
+//$response2 = $cards->findById("card_6d502929-c214-4c3f-8e4d-191bd42edafa");
 
 //$decode = json_decode($response2);
 
-print_r($response2['name']);
-echo "<p>";
-print_r($response2['year']);
+//print_r($response2['name']);
+//echo "<p>";
+//print_r($response2['year']);
 
 //echo ($response2);
 
@@ -39,25 +42,25 @@ print_r($response2['year']);
 $card = new Card();
 $card->setCustomerId("customer_7241f2c6-d8d7-4648-9843-e494c1ac881b");
 $card->setName("Est luminus");
-$card->setNumber("44445555555555555");
+$card->setNumber("5514967494092156");
 $card->setMonth("6");
 $card->setYear("19");
 $card->setCvc("122");
 $card->setCardAlias("Favorite Platium card");
-$card->setValidate(false);
+//$card->setValidate(false);
 
 $metadata = array("internal_id"=>"84372");
 
-$card->setMetadata($metadata);
+//$card->setMetadata($metadata);
 
-$address = array("street"=>"Losten Street, 192", "city"=>"Yahar'gul","state"=>"YX");
-$card->setAddress($address);
+///$address = array("street"=>"Losten Street, 192", "city"=>"Yahar'gul","state"=>"YX");
+//$card->setAddress($address);
+
 
 //$json = json_encode($card);
-//print_r ($json);
 
 
-//$cards->create($json);
+echo $cards->create($card->jsonSerialize());
 
 //valido
 //card_6d502929-c214-4c3f-8e4d-191bd42edafa
