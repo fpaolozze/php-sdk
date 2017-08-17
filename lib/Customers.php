be38ed0d0eddaf4b46f0b40f36f5a2120a4b747b
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-17 12:26:53 -0300

    removed traits construct

--- lib/Customers.php
+++ lib/Customers.php
@@ -15,18 +15,10 @@ class Customers
     public $card;
 
     use findAll, findById, insert, update {
-        findAll::__construct as private __findConstruct;
-        //findById::__construct as private __findByIdConstruct;
-        insert::__construct as private __insertConstruct;
-        update::__construct as private __updateConstruct;
     }
 
     public function __construct()
     {
-        $this->__findConstruct();
-        //$this->__findByIdConstruct();
-        $this->__insertConstruct();
-        $this->__updateConstruct();
     }
 
     public function _set($properties){
