3685bce9b2cabfdfed685c4be1f8fa53123ca83c
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-17 12:32:58 -0300

    make parameters public

--- lib/model/Address.php
+++ lib/model/Address.php
@@ -3,103 +3,17 @@ namespace Paggi\model;
 
 class Address implements \JsonSerializable {
 
-    private $street;
-    private $neighborhood;
-    private $city;
-    private $state;
-    private $zip;
+    public $street;
+    public $neighborhood;
+    public $city;
+    public $state;
+    public $zip;
 
-    public function __construct()
-    {
-    }
 
     public function jsonSerialize()
     {
         return get_object_vars($this);
     }
-
-    /**
-     * @return mixed
-     */
-    public function getStreet()
-    {
-        return $this->street;
-    }
-
-    /**
-     * @param mixed $street
-     */
-    public function setStreet($street)
-    {
-        $this->street = $street;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getNeighborhood()
-    {
-        return $this->neighborhood;
-    }
-
-    /**
-     * @param mixed $neighborhood
-     */
-    public function setNeighborhood($neighborhood)
-    {
-        $this->neighborhood = $neighborhood;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCity()
-    {
-        return $this->city;
-    }
-
-    /**
-     * @param mixed $city
-     */
-    public function setCity($city)
-    {
-        $this->city = $city;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getState()
-    {
-        return $this->state;
-    }
-
-    /**
-     * @param mixed $state
-     */
-    public function setState($state)
-    {
-        $this->state = $state;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getZip()
-    {
-        return $this->zip;
-    }
-
-    /**
-     * @param mixed $zip
-     */
-    public function setZip($zip)
-    {
-        $this->zip = $zip;
-    }
-
-
-
 }
 
 
