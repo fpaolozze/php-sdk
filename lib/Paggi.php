610bdea1ff8f7f97e9c33f4e553731d61ea73d7a
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-18 02:06:29 -0300

    Changed now it receive the token and enviroment on constructor, it was necessary because the curl/restclient must be instancied on constructor

--- lib/Paggi.php
+++ lib/Paggi.php
@@ -9,26 +9,48 @@ include 'lib/RestClient.php';
  */
 class Paggi
 {
-  static $isStaging;
-  static $token;
+    static private $isStaging;
+    static private $token;
 
-  static public function init($_token,$_isStaging)
-  {
-    self::$token = $_token;
-    self::$isStaging = $_isStaging;
-  }
+    private $restClient;
 
-  static public function getToken(){
-    return self::$token;
-  }
+    private $cards;
+    protected $bank_accounts;
 
-  static public function isStaging(){
-    return self::$isStaging;
-  }
+
+    public function __construct($token, $staging = false)
+    {
+        self::$isStaging = $staging;
+        self::$token = $token;
+        $this->restClient = new RestClient();
+
+    }
+
+    static public function getToken()
+    {
+        return self::$token;
+    }
+
+    static public function isStaging()
+    {
+        return self::$isStaging;
+    }
+
+    public function cards(){
+        if(!$this->cards instanceof Cards){
+            return $this->cards = new Cards($this->restClient);
+        }
+    }
+
+    public function bank_accounts(){
+        if(!$this->bank_accounts instanceof Bank_accounts){
+            return $this->bank_accounts = new Bank_accounts($this->restClient);
+        }
+    }
+
 
 
 }
-
 
 
 ?>
