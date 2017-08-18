6d4b524a1845afcc8406e35fc0af8de5809753c8
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-18 02:02:26 -0300

    check if key response exists on class vars (key_exists)

--- lib/ModelBuild.php
+++ lib/ModelBuild.php
@@ -4,32 +4,11 @@ namespace Paggi;
 
 trait ModelBuild
 {
-
-    protected function build($parameters)
-    {
-
-        //echo json_encode($parameters);
-
-        /*if(is_array($parameters)){
-            print_r("ARRAY");
-        }else{
-            print_r("nnot array");
-        }*/
-
-        $classe = new \ReflectionObject($this);
+    protected function buildObject($properties){
         $class_var = get_class_vars(get_class($this));
-
-
-        foreach ($parameters as $key => $value) {
-            if (in_array($key, $class_var)) {
-                print_r($class_var);
-                print_r($value);
-                $this->{$key} = "kkk";
-            //if (!in_array($key, $parameters)) {
-                //$this->{$key} = $value;
-                //foreach ($key as $objeto){
-                    //print_r($value);
-                //}
+        foreach($properties as $key => $value){
+            if (key_exists($key, $class_var)) {//key_exists
+                $this->{$key} = $value;
             }
         }
     }
