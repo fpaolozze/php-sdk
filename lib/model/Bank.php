69731beb5b50d57ad41488437e14c99a92c5d039
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-18 15:42:18 -0300

    model Bank

--- lib/model/Bank.php
+++ lib/model/Bank.php
@@ -2,10 +2,22 @@
 
 namespace Paggi;
 
-class Bank{
+class Bank implements \JsonSerializable {
+
+    use ModelBuild;
 
     public $id;
     public $name;
+
+    public function __construct($response)
+    {
+        $this->buildObject($response);
+    }
+
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
+    }
 }
 
 
