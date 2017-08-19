cef22ccef02c34a099de562df0b0e65676b5d143
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-19 20:46:16 -0300

    add charge, cancl ad capture

--- lib/Charges.php
+++ lib/Charges.php
@@ -4,44 +4,32 @@ namespace Paggi;
 
 class Charges
 {
-    use findAll, findById, charge, insert,ModelBuild{
-        charge::__construct as private __chargeConstruct;
-    }
+    use Util;
+    use findAll;
+    use findById;
+    use charge_;
+    use insert;
 
-    public $total;
-    public $result;
+    private $restClient;
 
-    //values to send
-    public $amount = 100;
-    public $customer_id = "customer_ab32d26f-f396-460e-a2c4-7963543055b4";
-    public $statement_descriptor;
-    public $capture;
-    public $destination;
-    public $risk_analysis;
-    public $installments_number;
-    public $force;
-
-    public $status;
-    public $receipt_email;
-    public $metadata;
-    public $id;
-    public $acquirer_message;
-    public $acquirer_code;
-    public $expected_compensation;
-    public $customer;
-    public $created;
-    public $installments;
-
-
-    public function __construct($parameters)// $isParam = true
+    public function __construct($restClient)// $isParam = true
     {
-        $this->__chargeConstruct();//ModelBuild::__construct();
-        $this->build($parameters);
+        $this->restClient = $restClient;
     }
 
-    static public function chargeStatic(){
-        
+    public function charge($body){
+        return $this->_charge($this->restClient,$body);
     }
+
+    public function cancel($chargeId)
+    {
+        return $this->_cancelCapture($this->restClient,$chargeId,"cancel");
+    }
+
+    public function capture($chargeId){
+        return $this->_cancelCapture($this->restClient,$chargeId,"capture");
+    }
+
 
 }
 
