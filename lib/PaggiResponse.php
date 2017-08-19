acb7cd597a0db47b82f2ee6bccc35235255a260e
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-19 20:47:01 -0300

    fix

--- lib/PaggiResponse.php
+++ lib/PaggiResponse.php
@@ -6,14 +6,20 @@ class PaggiResponse{
 
     use \Paggi\ModelBuild;
 
+    public $errors;
+    public function __construct($resul)
+    {
+        $this->buildObject($resul);
+    }
+
+}
+
+
+class Errors{
+
     public $code;
     public $message;
     public $type;
-
-    public function __construct($errors)
-    {
-        $this->buildObject($errors);
-    }
 
 }
 ?>
