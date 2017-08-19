b6d7ff0abb3bcb5541f0c3046d2639163591752f
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-19 20:47:32 -0300

    some tests with array response

--- lib/ModelBuild.php
+++ lib/ModelBuild.php
@@ -2,14 +2,36 @@
 
 namespace Paggi;
 
+use Paggi\model\Bank;
+
 trait ModelBuild
 {
-    protected function buildObject($properties){
+    //key_exists - in_array
+
+    protected function buildObject1($properties){
+        //var_dump($properties);
+        $class_var = get_class_vars(get_class($this));
+        foreach($properties as $obj){
+            //if (in_array($key, $class_var)) {
+                foreach ($obj as $key => $object){
+                    //array_push($class_var,$object);
+                    //print_r($class_var);
+                    //var_dump($object."<p>");
+
+                }
+            //}
+        }
+    }
+
+
+    private function buildObject($properties){
         $class_var = get_class_vars(get_class($this));
         foreach($properties as $key => $value){
-            if (key_exists($key, $class_var)) {//key_exists
-                $this->{$key} = $value;
-            }
+            //if (key_exists($key, $class_var)) {//key_exists - in_array array_key_exists
+                if(!is_null($value)) {
+                    $this->{$key} = $value;
+                }
+            //}
         }
     }
 }
