6274e9c02dac42e49a515f66c2aae257a048867c
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-21 11:18:59 -0300

    BankAcounts static parameters

---
+++ lib/BankAccountsParameters.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Paggi;
+
+/**
+ * Class BankAccountsParameters
+ * @package Paggi
+ */
+class BankAccountsParameters{
+
+    const BANK_ID = "bank_id";
+    const CUSTOMER_ID = "customer_id";
+    const NUMBER = "number";
+    const DIGIT = "digit";
+    const BRANCH = "branch";
+    const BRANCH_DIGIT = "branch_digit";
+
+
+}
+
+?>
