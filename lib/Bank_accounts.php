28d8e63e20a382f494b996ce66df183f51f0938f
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-20 21:46:12 -0300

    removed the vars; refactor actions methods
    The methods returns a BankAcount or a BankAccountCatalog

--- lib/Bank_accounts.php
+++ lib/Bank_accounts.php
@@ -2,29 +2,40 @@
 
 namespace Paggi;
 
+use Paggi\model\BankAccount;
+use Paggi\model\BankAccountCatalog;
+
 class Bank_accounts
 {
-    use insert, findAll, findById, update, ModelBuild;
+    use Util; //The methods uses methods from Util
 
-    public $total;
-    public $result = array();
+    use insert, findAll, findById, update;
 
-    public $id;
-    public $customer_id;
-    public $bank;
-    public $number;
-    public $digit ;
-    public $branch ;
-    public $branch_digit;
-    public $bank_id;
+    private $restClient;
 
-    public function __construct($parameters)
+    public function __construct()
     {
-        $this->build($parameters);
+        $this->restClient = new RestClient();
     }
 
-    public function save(){
+    public function create($params){
+        $response = $this->_create($this->restClient, $params);
+        return new BankAccount($response);
+    }
 
+    public function findAll($query_params = []){
+        $response = $this->_findAll($this->restClient,$query_params);
+        return new BankAccountCatalog($response);
+    }
+
+    public function findById($bank_account_id){
+        $response = $this->_findById($this->restClient,$bank_account_id);
+        return new BankAccount($response);
+    }
+
+    public function update($bank_account_id, $body_params){
+        $response = $this->_update($this->restClient,$bank_account_id,$body_params);
+        return new BankAccount($response);
     }
 }
 
