f4c759cef6939cff3a397bc5899bcd9c2a2db71a
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-21 13:19:45 -0300

    receive the restClient in constructor

--- lib/Bank_accounts.php
+++ lib/Bank_accounts.php
@@ -7,15 +7,17 @@ use Paggi\model\BankAccountCatalog;
 
 class Bank_accounts
 {
-    use Util; //The methods uses methods from Util
+    //The methods uses methods from Util
+    use Util;
 
+    //Actions for this resource
     use insert, findAll, findById, update;
 
     private $restClient;
 
-    public function __construct()
+    public function __construct($restClient)
     {
-        $this->restClient = new RestClient();
+        $this->restClient = $restClient;
     }
 
     /**
@@ -28,16 +30,32 @@ class Bank_accounts
         return new BankAccount($response);
     }
 
+    /**
+     * List all banks accounts
+     * @param array Query params to pagination
+     * @return BankAccountCatalog
+     */
     public function findAll($query_params = []){
         $response = $this->_findAll($this->restClient,$query_params);
         return new BankAccountCatalog($response);
     }
 
+    /**
+     * Retrieves a Bank account by id
+     * @param Bank account id
+     * @return BankAccount
+     */
     public function findById($bank_account_id){
         $response = $this->_findById($this->restClient,$bank_account_id);
         return new BankAccount($response);
     }
 
+    /**
+     * Update a bank account
+     * @param $bank_account_id Bank
+     * @param $body_params
+     * @return BankAccount
+     */
     public function update($bank_account_id, $body_params){
         $response = $this->_update($this->restClient,$bank_account_id,$body_params);
         return new BankAccount($response);
