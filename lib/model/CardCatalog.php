0fca1a9a6ad575e37e5358c123103c7043f036fa
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-20 21:50:07 -0300

    refactor to CardCatalog of a list Cards.

--- lib/model/CardCatalog.php
+++ lib/model/CardCatalog.php
@@ -2,39 +2,15 @@
 
 namespace Paggi\model;
 
-use Paggi\ModelBuild;
-
-class Charge{
-
-    use ModelBuild;
+class CardCatalog{
 
     public $total;
     public $result = array();
-    public $customer_id;
-    public $capture = false;
-    public $receipt_email;
-    public $created;
-    public $force;
-
-    public $status;
-    public $statement_descriptor;
-    public $risk_analysis;
-    public $metadata;
-    public $installments_number;
-    public $installments = array();
-    public $id;
-    public $expected_compensation;
-    public $destination;
-    public $customer;
-    public $created_at;
-    public $amount;
-    public $acquirer_message;
-    public $acquirer_code;
-
 
     public function __construct($response)
     {
-        $this->buildObject($response);
+        $this->result = $response;
+        $this->total = sizeof($response);
     }
 }
 
