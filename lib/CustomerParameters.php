8475b8bc1082c13f86cb333f1d6c79d55d4dca48
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-21 11:36:14 -0300

    CustomerParameters

---
+++ lib/CustomerParameters.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Paggi;
+
+class CustomerParameters{
+
+    const NAME = "name";
+    const EMAIL = "email";
+    const DOCUMENT = "document";
+    const PHONE = "phone";
+    const DESCRIPTION = "description";
+    const METADATA = "metadata";
+    const ADDRESS = "address";
+    const CARD = "card";
+
+}
+
+?>
