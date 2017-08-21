6350cad418a1cd7f745446f6132b7b8eca0f32fd
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-20 21:52:42 -0300

    installments model

---
+++ lib/model/Installments.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Paggi\Model;
+
+class Installments{
+
+    /**
+     * @var string
+     */
+    public $status;
+    /**
+     * @var int
+     */
+    public $number;
+    /**
+     * @var string
+     */
+    public $expected_date;
+    /**
+     * @var float
+     */
+    public $amount;
+}
+
+?>
