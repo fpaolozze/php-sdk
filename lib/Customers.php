e42567c5a555d8f74712df19543cf9ecd8f534bf
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-22 14:33:25 -0300

    remove findById and update

--- lib/Customers.php
+++ lib/Customers.php
@@ -9,27 +9,17 @@ class Customers
 {
     use Util; //The methods uses methods from Util
 
-    use findAll, findById, insert, update; //Actions
+    use findAll, insert; //Actions
 
-    private $restClient;
-
-    /**
-     * Customers constructor. Restclient manage the curl
-     * @param $restClient
-     */
-    public function __construct($restClient)
-    {
-        $this->restClient = $restClient;
-    }
 
     /**
      * Store new clients with or without card and address data
      * @param $parameters Customer parameters
      * @return Customer model
      */
-    public function create($parameters)
+    static public function create($parameters)
     {
-        $response = $this->_create($this->restClient, $parameters);
+        $response = self::traitCreate($parameters);
         return new Customer($response);
     }
 
@@ -38,34 +28,13 @@ class Customers
      * @param array $queryParams QUery to filter pagination
      * @return CustomerCatalog model
      */
-    public function findAll($queryParams = [])
+    static public function findAll($queryParams = [])
     {
-        $response = $this->_findAll($this->restClient, $queryParams);
+        $response = self::traitFindAll($queryParams);
         return new CustomerCatalog($response);
     }
 
-    /**
-     * Retrieves a customer by Customer id
-     * @param $customerId
-     * @return Customer model
-     */
-    public function findById($customerId)
-    {
-        $response = $this->_findById($this->restClient, $customerId);
-        return new Customer($response);
-    }
 
-    /**
-     * Update a customer
-     * @param $customerId Customer id
-     * @param array $parameters Customer parameters
-     * @return Customer model
-     */
-    public function update($customerId, $parameters = [])
-    {
-        $response = $this->_update($this->restClient, $customerId, $parameters);
-        return new Customer($response);
-    }
 }
 
 ?>
