c969a3bad951db63223bb63cb0a6408b920de1ee
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-22 18:22:02 -0300

    return conform 'staging return' not api document return

--- lib/model/Bank.php
+++ lib/model/Bank.php
@@ -2,6 +2,9 @@
 
 namespace Paggi\model;
 
+use Paggi\ModelBuild;
+use Paggi\Paggi;
+
 /**
  * Class Bank - Represents a Bank catalog
  * @package Paggi\model
@@ -13,13 +16,24 @@ class Bank
 
     public function __construct($response)
     {
-        if(!is_null($response)) {
+        /*        if(!is_null($response)) {
+                    $this->total = sizeof($response);
+                    foreach ($response as $objeto) {
+                        $banco = new BankObj();
+                        $banco->name = $objeto['name'];
+                        $banco->id = $objeto['id'];
+                        array_push($this->result, $banco);
+                    }
+                }*/
+
+        if (!is_null($response)) {
             $this->total = sizeof($response);
-            foreach ($response as $objeto) {
-                $banco = new BankObj();
-                $banco->name = $objeto['name'];
-                $banco->id = $objeto['id'];
-                array_push($this->result, $banco);
+
+            $resut = $response['result'];
+
+            foreach ($resut as $value) {
+                $bank = new BankObj($value);
+                array_push($this->result, $bank);
             }
         }
     }
@@ -31,8 +45,17 @@ class Bank
  */
 class BankObj
 {
+
+    use ModelBuild;
+
+
     public $name;
     public $id;
+
+    public function __construct($parameters)
+    {
+        $this->buildObject($parameters);
+    }
 }
 
 
