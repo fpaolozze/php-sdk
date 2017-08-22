34d1623fa61ba3548483c93fc4dcc8002c48e3bc
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-22 14:36:36 -0300

    remove findById and Update

--- lib/Bank_accounts.php
+++ lib/Bank_accounts.php
@@ -11,7 +11,7 @@ class Bank_accounts
     use Util;
 
     //Actions for this resource
-    use insert, findAll, findById, update;
+    use insert, findAll;
 
     private $restClient;
 
@@ -29,8 +29,8 @@ class Bank_accounts
      * @param $params
      * @return BankAccount
      */
-    public function create($params){
-        $response = $this->_create($this->restClient, $params);
+    static public function create($params){
+        $response = self::traitCreate($params);
         return new BankAccount($response);
     }
 
@@ -39,30 +39,10 @@ class Bank_accounts
      * @param array Query params to pagination
      * @return BankAccountCatalog
      */
-    public function findAll($query_params = []){
-        $response = $this->_findAll($this->restClient,$query_params);
+    static public function findAll($query_params = [])
+    {
+        $response = self::traitFindAll($query_params);
         return new BankAccountCatalog($response);
-    }
-
-    /**
-     * Retrieves a Bank account by id
-     * @param Bank account id
-     * @return BankAccount
-     */
-    public function findById($bank_account_id){
-        $response = $this->_findById($this->restClient,$bank_account_id);
-        return new BankAccount($response);
-    }
-
-    /**
-     * Update a bank account
-     * @param $bank_account_id Bank
-     * @param $body_params
-     * @return BankAccount
-     */
-    public function update($bank_account_id, $body_params = []){
-        $response = $this->_update($this->restClient,$bank_account_id,$body_params);
-        return new BankAccount($response);
     }
 }
 
