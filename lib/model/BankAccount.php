3064e08dfe1222d1534f30cc36deca3fc7a64446
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-22 14:35:20 -0300

    add findById and update method

--- lib/model/BankAccount.php
+++ lib/model/BankAccount.php
@@ -2,7 +2,10 @@
 
 namespace Paggi\Model;
 
+use Paggi\findById;
 use Paggi\ModelBuild;
+use Paggi\update;
+use Paggi\Util;
 
 /**
  * Class BankAccount
@@ -11,6 +14,10 @@ use Paggi\ModelBuild;
 class BankAccount{
 
     use ModelBuild;
+
+    use Util;
+
+    use update, findById;
 
     public $id;
     public $customer_id;
@@ -23,6 +30,28 @@ class BankAccount{
     public function __construct($response)
     {
         $this->buildObject($response);
+    }
+
+
+    /**
+     * Retrieves a Bank account by id
+     * @param Bank account id
+     * @return BankAccount
+     */
+    public function findById(){
+        $response = self::traitFindById();
+        return new BankAccount($response);
+    }
+
+    /**
+     * Update a bank account
+     * @param $bank_account_id Bank
+     * @param $body_params
+     * @return BankAccount
+     */
+    public function update($body_params = []){
+        $response = self::update($body_params);
+        return new BankAccount($response);
     }
 
 }
