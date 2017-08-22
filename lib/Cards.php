6d231f5f5030e6f76cf928b16633547f3b0f1536
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-08-22 11:54:08 -0300

    removed  delete;findById.

--- lib/Cards.php
+++ lib/Cards.php
@@ -11,66 +11,30 @@ class Cards
 
     use Util; //The methods uses methods from Util
 
-    /**
-     * Insert a new card
-     * Find a card by id
-     * Find all cards
-     * Delete a card
-     */
-    use insert, findById, findAll, delete;
+    use insert, findAll;
 
-    /**
-     * Cards constructor. Instance the RestClient object. The curl/restclient must be initilized on the constructor;
-     * @param $restClient Get from Paggi class
-     */
-    public function __construct($restClient)
-    {
-        $this->restClient = $restClient;
-    }
+
 
     /**
      * Create a new card. You can store multiple cards for each customer, in order to charge the customer later.
      * @param $params body params
-     * @return Card A card object
+     * @return Cards A card object
      */
-    public function createCard($params)
+    static public function create($params)
     {
-        $response = $this->_create($this->restClient, $params);
+        $response = self::traitCreate($params);
         return new Card($response);
     }
 
     /** List all cards with pagination and creation date filter
      * @param array $query_params query pagination and filter
-     * @return Card object
+     * @return Cards object
      */
-    public function findAll($query_params = [])
+    static public function findAll($query_params = [])
     {
-        $response = $this->_findAll($this->restClient, $query_params);
+        $response = self::traitFindAll($query_params);
         return new CardCatalog($response);
     }
-
-    /**
-     * Delete a card
-     * @param $card_id The card id to remove
-     * @return Thee removed card object
-     */
-    public function delete($card_id)
-    {
-        $response = $this->_delete($this->restClient, $card_id);
-        return new Card($response);
-    }
-
-    /** Retrieves a card
-     * @param $card_id The card_id to find
-     * @return Card object
-     */
-    public function findById($card_id)
-    {
-        $response = $this->_findById($this->restClient, $card_id);
-        return new Card($response);
-    }
-
-
 
 }
 
