ff185af0d9abd72ff16f24d1efdc5f5b4d025758
Rafael Felipe <rafael.felipe1989@gmail.com>
2017-09-26 18:30:39 -0300

    implements capture/cancel on charge

--- lib/Charge.php
+++ lib/Charge.php
@@ -7,10 +7,12 @@ use Paggi\Entity;
 use Paggi\Traits\Create;
 use Paggi\Traits\FindAll;
 use Paggi\Traits\FindById;
+use Paggi\Traits\Cancel;
+use Paggi\Traits\Capture;
 
 class Charge extends Entity
 {
-    use FindAll, Create, FindById;
+    use FindAll, Create, FindById, Cancel, Capture;
 }
 
 ?>
